9f1e901163cd87c47b0805083512f8df57aa73f5
Rafal Bielski <rafbiels@gmail.com>
2021-04-24 23:18:19 +0200

    Add some styling for Recipe component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/scss/bootstrap.scss'
+import './styles/style.scss';
+import './styles/minty/variables.scss';
 import {RecipeList} from './components/recipe-list.component'
 
 function App() {
--- frontend/src/components/recipe.component.tsx
+++ frontend/src/components/recipe.component.tsx
@@ -14,14 +14,15 @@ interface Props {
 export const Recipe = ({recipe}: Props) => {
   const itemList = [];
   for (let i=0; i<recipe.items.length; ++i) {
-    itemList.push( <li>{recipe.items[i]} x{recipe.amounts[i]}</li> );
+    itemList.push( <li key={"item"+i}>{recipe.items[i]} x{recipe.amounts[i]}</li> );
   }
   return (
-    <div>
-      <h2>Recipe:</h2>
-      <h3>{recipe.name}</h3>
-      <h5>{recipe.description}</h5>
-      <ul>{itemList}</ul>
+    <div className="card recipe shadow-sm my-2 mx-auto p-0">
+      <div className="card-body p-0">
+        <h3 className="card-title bg-light py-1 px-3">{recipe.name}</h3>
+        <h5 className="card-subtitle my-1 mx-3">{recipe.description}</h5>
+        <ul className="card-text my-2 mx-1">{itemList}</ul>
+      </div>
     </div>
   );
 };
